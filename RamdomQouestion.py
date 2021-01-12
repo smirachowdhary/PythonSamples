1bed3e2f20d46a41c098a705124de66d6219dba9
Smira C <76918800+smirachowdhary@users.noreply.github.com>
2021-01-12 08:58:47 -0800

    Save 3 Assignment 2

--- RamdomQouestion.py
+++ RamdomQouestion.py
@@ -1,23 +1,24 @@
 import random
-
+#
 n1 = random.randint(1,20)
 n2 = random.randint(100,1000)
-
+#
 print (f"{n1} * {n2}")
 answer = int(input(""))
-
+#
 if n1 * n2 == answer:
     print ("Great Job! Your answer is correct.")
 else:
     print(f"Sorry, you answer is not correct. The real answer is {n1 * n2}.")
-
+#
 n2 = random.randint(1,20)
 n1 = random.randint(100,1000)
-
+#
 print (f"{n1} / {n2}")
 answer = int(input(""))
-
+#
 if n1 / n2 == answer:
     print ("Great Job! Your answer is correct.")
 else:
     print(f"Sorry, you answer is not correct. The real answer is {n1 / n2}.")
+#
