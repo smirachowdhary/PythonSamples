5c57ef8e7be3a8954a3a11206d6a388fa9142215
Smira C <76918800+smirachowdhary@users.noreply.github.com>
2021-01-17 14:27:03 -0800

    Exercise and Study Drills 3

---
+++ Exercise3.py
@@ -0,0 +1,23 @@
+print("I will now count my chickens:") # It is printing I will now count my chickens:
+
+print("Hens", 25 + 30 / 6) # It is printing the number of hens.
+print("Roosters", 100 - 25 * 3 % 4) # It is printing the number of roosters.
+
+print("Now I will count the eggs:") # It is printing Now I will count the eggs:
+
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6) # It is printing the number of eggs.
+
+print("Is it true that 3 + 2 < 5 - 7?") # Asking a question.
+
+print(3 + 2 < 5 - 7) # Answering a question.
+
+print("What is 3 + 2?",3 + 2) # Answering a question.
+print("What is 5 - 7?",5 - 7) # Answering a question.
+
+print("Oh, that's why it is False.") # It is printing Oh, that's why it is False.
+
+print("How about some more.") # It is printing How about some more.
+
+print("Is 5 > -2 greater?",5 > -2) # Asking and answering a question.
+print("Is 5 >= -2 greater or equal?",5 >= -2) # Asking and answering a question.
+print("Is 5 <= -2 less or equal?",5 <= -2) # Asking and answering a question.
---
+++ StudyDrills3.py
@@ -0,0 +1,4 @@
+# Above each line, use the # to write a comment to yourself explaining what the line does. 
+# Remember in Exercise 0 when you started python3.6? Start python3.6 this way again and, using the math operators, use Python as a calculator. Done.
+# Find something you need to calculate and write a new .py file that does it. Done.
+# Rewrite ex3.py to use floating point numbers so it's more accurate. 20.0 is floating point. Done.
