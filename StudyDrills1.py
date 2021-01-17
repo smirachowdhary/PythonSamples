e56f9e387a8ec56327229c69f93050c060a52333
Smira C <76918800+smirachowdhary@users.noreply.github.com>
2021-01-17 10:11:26 -0800

    Study Drills 1

---
+++ StudyDrills1.py
@@ -0,0 +1,3 @@
+# Make your script print another line. Done.
+# Make your script print only one of the lines. Done.
+# Put a # (octothorpe) character at the beginning of a line. What did it do? Try to find out what this character does. Anwser: the character # hash makes a line unrunable - it makes the line not run.
