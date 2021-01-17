0153759c1c94305584f2716069612dcc2e39a3ec
Smira C <76918800+smirachowdhary@users.noreply.github.com>
2021-01-17 11:33:58 -0800

    Exercise and Study Drills 2

---
+++ Exercise2.py
@@ -0,0 +1,12 @@
+# A comment, this is so you can read your program later.
+# Anything after the # is ignored by python.
+
+print("I could have code like this.") # and a comment after is ignored
+
+# You can also use a comment to "disable" or or comment out code:
+# print("This won't run.")
+
+print("This will run.")
+
+print("Hello!")
+# print("Hello!") I was right. When I ran this, it only printed 1 Hello! not 2.
---
+++ StudyDrills2.py
@@ -0,0 +1,4 @@
+# Find out if you were right about what the # character does and make sure you know what it's called (octothorpe or pound character). Done.
+# Take your ex2.py file and review each line going backward. Start at the last line, and check each word in reverse against what you should have typed. Done.
+# Did you find more mistakes? Fix them. Done.
+# Read what you typed above out loud, including saying each character by its name. Did you find more mistakes? Fix them. Done.
