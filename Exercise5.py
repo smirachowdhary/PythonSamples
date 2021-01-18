2800ce33e5e5c12174bcbc9f5c1a58ec884fe2b4
Smira C <76918800+smirachowdhary@users.noreply.github.com>
2021-01-18 10:43:37 -0800

    Exercise and Study Drills 5

---
+++ Exercise5.py
@@ -0,0 +1,20 @@
+name = Smira C.
+age = 10
+height = 55 # inches
+weight = 80 # pounds
+eyes = dark, dark brown
+teeth = white
+hair = black
+
+print(f"Let's talk about {name}.")
+print(f"She's {height} inches tall.")
+print(f"She's {weight} pounds heavy.")
+print("Actually, that's not to heavy.")
+print(f"She's got {eyes} eyes and {hair} hair.")
+print(f"Her teeth are usually {teeth} depening on the type of coffee.")
+
+total = age + height + weight
+print(f"If I add {age}, {height}, and {weight} I get {total}.")
+
+height_in_centimeters = height * 2.54
+weight_in_kilograms = weight * 2.205
---
+++ StudyDrills5.py
@@ -0,0 +1,4 @@
+# Change all the variables so there is no my_ in front of each one. Make sure you change the name everywhere, not just where you used = to set them.
+# Done.
+# Try to write some variables that convert the inches and pounds to centimeters and kilograms. Do not just type in the measurements. Work out the math in Python.
+# Done.
