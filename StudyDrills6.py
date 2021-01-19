20e96863c1e1f0fee5a55c5bc32edbff09c02761
Smira C <76918800+smirachowdhary@users.noreply.github.com>
2021-01-18 17:16:02 -0800

    Exercise and Study Drills 6

---
+++ StudyDrills6.py
@@ -0,0 +1,8 @@
+# Go through this program and write a comment above each line explaining it.
+# Done.
+# Find all the places where a string is put inside a string.
+# In lines 6, 8, 9, 11, and 12.
+# Are you sure there are only four places? How do you know? Maybe I like lying.
+# There are really five.
+# Explain why adding the two strings w and e with + makes a longer string.
+# Adding the two strings w and e with + makes a longer string because you are addin two strings which will combine the 2 strrings which will make one longer string attaching them to each other, frist then last string.
