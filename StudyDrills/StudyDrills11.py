bcacc65bc9872a1de1ff5dbcabe459a3e8286d5b
Smira C <76918800+smirachowdhary@users.noreply.github.com>
2021-01-19 14:55:21 -0800

    Exercise and Study Drills 11

---
+++ StudyDrills/StudyDrills11.py
@@ -0,0 +1,6 @@
+# Go online and find out what Python’s input does.
+# Python has an input function which lets you ask a user for some text input.
+# Can you find other ways to use it? Try some of the samples you find.
+# No.
+# Write another “form” like this to ask some other questions.
+# Done-It is a calaculator.
