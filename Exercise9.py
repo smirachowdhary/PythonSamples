dea5132f1af31ce87d522b42c9c49c0edd362e79
Smira C <76918800+smirachowdhary@users.noreply.github.com>
2021-01-19 10:47:40 -0800

    Exercise and Study Drills 9

---
+++ Exercise9.py
@@ -0,0 +1,12 @@
+days = "Monday Tuesday Wednesday Friday Saturday Sunday"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug\nSep\nOct\nNov\nDec"
+
+print("Here are the days:", days)
+print("Here are the months:", months)
+
+print("""
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+""")
