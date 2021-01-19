95b1f62ad036d1be6792e03f3b86457e3f0c8f98
Smira C <76918800+smirachowdhary@users.noreply.github.com>
2021-01-19 08:04:49 -0800

    Exercise and Study Drills 8

---
+++ Exercise8.py
@@ -0,0 +1,6 @@
+formatter = "{} {} {} {}"
+
+print(formatter.format(1,2,3,4))
+print(formatter.format("one", "two", "three", "four"))
+print(formatter.format(True, False, False, True))
+print(formatter.format("Hello", " Mom,", " Dad,", "and Viraj."))
---
+++ StudyDrills8.py
@@ -0,0 +1,10 @@
+# Go back through and write a comment on what each line does.
+# Done.
+# Read each one backward or out loud to find your errors.
+# Done.
+# From now on, when you make mistakes, write down on a piece of paper what kind of mistake you made.
+# Done.
+# When you go to the next exercise, look at the mistakes you have made and try not to make them in this new one.
+# Done.
+# Remember that everyone makes mistakes. Programmers are like magicians who fool everyone into thinking they are perfect and never wrong, but it's all an act. They make mistakes all the time.
+# Done.
