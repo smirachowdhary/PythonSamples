b757b1fb2a537a861f571a644a4115262b1cd0ff
Smira C <76918800+smirachowdhary@users.noreply.github.com>
2021-01-20 08:27:19 -0800

    Exercise and Study Drills 13

--- StudyDrills/StudyDrills13.py
+++ StudyDrills/StudyDrills13.py
@@ -1,8 +1,8 @@
-from sys import argv
-
-script, first, second, third = argv, argv, argv, argv
-
-print("The script is called:", script)
-print("Your first varible is:", first)
-print("Your second varible is:", second)
-print("Your third varible is:", third)
+# Try giving fewer than three arguments to your script. See that error you get? See if you can explain it.
+# Done.
+# Write a script that has fewer arguments and one that has more. Make sure you give the unpacked variables good names.
+# Done.
+# Combine input with argv to make a script that gets more input from a user. Don’t overthink it. Just use argv to get something, and input to get something else from the user.
+# Done.
+# Remember that modules give you features. Modules. Modules. Remember this because we’ll need it later.
+# Done.
