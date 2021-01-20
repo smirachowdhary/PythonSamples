71cec7fa084bfde38426cd34bd78400951b69c2e
Smira C <76918800+smirachowdhary@users.noreply.github.com>
2021-01-20 08:01:00 -0800

    Exercise and Study Drills 12

---
+++ StudyDrills/StudyDrills12.py
@@ -0,0 +1,8 @@
+# In Terminal, where you normally run python3.6 to run your scripts, type pydoc input. Read what it says. If you’re on Windows try python3.6 -m pydoc input instead.
+# Done.
+# Get out of pydoc by typing q to quit.
+# Done.
+# Look online for what the pydoc command does.
+# Done.
+# Use pydoc to also read about open, file, os, and sys. It’s alright if you do not understand those; just read through and take notes about interesting things.
+# Done.
