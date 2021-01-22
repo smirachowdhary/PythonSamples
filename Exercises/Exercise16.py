a115a68615ff368560336e282572d84f8176f66f
Smira C <76918800+smirachowdhary@users.noreply.github.com>
2021-01-22 09:31:20 -0800

    Creating files for today

--- Exercises/Exercise16.py
+++ Exercises/Exercise16.py
@@ -1,8 +1,7 @@
 from sys import argv
 
-script, first, second, third = argv, argv, argv, argv
+script, filename = argv, argv
 
-print("The script is called:", script)
-print("Your first varible is:", first)
-print("Your second varible is:", second)
-print("Your third varible is:", third)
+print(f"")
+print("")
+print("")
--- StudyDrills/StudyDrills17.py
+++
@@ -1,8 +0,0 @@
-# Find out what the games Zork and Adventure were. Try to find copies and play them.
-# ok.
-# Change the prompt variable to something else entirely.
-# Done.
-# Add another argument and use it in your script, the same way you did in the previous exercise with first, second = ARGV.
-# Ok.
-# Make sure you understand how I combined a """ style multiline string with the {} format activator as the last print
-# Ok.
