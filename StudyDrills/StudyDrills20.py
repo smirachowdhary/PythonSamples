6088e99849c8c5ffbddbd212fa51860fa31053df
Smira C <76918800+smirachowdhary@users.noreply.github.com>
2021-01-27 15:59:59 -0800

    Exercise and Study Drills 20

---
+++ StudyDrills/StudyDrills20.py
@@ -0,0 +1,10 @@
+# Write an English comment for each line to understand what that line does.
+# Done.
+# Each time print_a_line is run, you are passing in a variable, current_line. Write out what current_line is equal to on each function call, and trace how it becomes line_count in print_a_line.
+# Done.
+# Find each place a function is used, and check its def to make sure that you are giving it the right arguments.
+# Done.
+# Research online what the seek function for file does. Try pydoc file, and see if you can figure it out from there. Then try pydoc file.seek to see what seek does.
+# Done.
+# Research the shorthand notation +=, and rewrite the script to use += instead.
+# Done.
