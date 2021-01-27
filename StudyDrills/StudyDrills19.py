beae2b2a43aeeffa00231badc4c3cb1bfd88cdee
Smira C <76918800+smirachowdhary@users.noreply.github.com>
2021-01-27 09:59:51 -0800

    Exercise and Study Drills 19

---
+++ StudyDrills/StudyDrills19.py
@@ -0,0 +1,6 @@
+# Go back through the script and type a comment above each line explaining in English what itdoes.
+# Done.
+# Start at the bottom and read each line backward, saying all the important characters.
+# Done.
+# Write at least one more function of your own design, and run it 10 different ways.
+# Done.
