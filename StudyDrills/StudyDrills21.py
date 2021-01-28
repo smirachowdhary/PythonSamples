c8375922d2600d3230dbbc7a108f9b25502bea43
Smira C <76918800+smirachowdhary@users.noreply.github.com>
2021-01-27 17:17:39 -0800

    Exercise and Study Drills 21

---
+++ StudyDrills/StudyDrills21.py
@@ -0,0 +1,8 @@
+# If you aren’t really sure what return does, try writing a few of your own functions and have them return some values. You can return anything that you can put to the right of an =.
+# Done.
+#  At the end of the script is a puzzle. I’m taking the return value of one function and using it as the argument of another function. I’m doing this in a chain so that I’m kind of creating a formula using the functions. It looks really weird, but if you run the script, you can see the results. What you should do is try to figure out the normal formula that would recreate this same set of operations.
+# Done.
+# Once you have the formula worked out for the puzzle, get in there and see what happens when you modify the parts of the functions. Try to change it on purpose to make another value.
+# Done.
+# Do the inverse. Write a simple formula and use the functions in the same way to calculate it.
+#Done.
