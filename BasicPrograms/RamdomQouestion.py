28cd845315ed9d464008718bb31f89d613710705
Smira C <76918800+smirachowdhary@users.noreply.github.com>
2021-02-23 07:45:39 -0800

    correction

--- BasicPrograms/RamdomQouestion.py
+++ BasicPrograms/RamdomQouestion.py
@@ -15,7 +15,7 @@ n2 = random.randint(1,20)
 n1 = random.randint(100,1000)
 #This makes to varibles for the question and the varibles are random in that varible.
 print (f"{n1} / {n2}")
-answer = int(input(""))
+answer = int(input())
 #This is giving the user a question and recording her/his answer.
 if n1 / n2 == answer:
     print ("Great Job! Your answer is correct.")
