0845b305770d924ab43a8e7d5ecd3ebf247d7683
Smira C <76918800+smirachowdhary@users.noreply.github.com>
2021-03-10 11:57:55 -0800

    shortcuts

--- BasicPrograms/BasicCoculaterProgram.py
+++ BasicPrograms/BasicCoculaterProgram.py
@@ -1,21 +1,21 @@
-TypeOfArthematic = input("Enter type of arthematic (addition, subtraction, multiplacation, division, and power):")
+TypeOfArthematic = input("Enter type of arthematic (a, s, m, d, and p):")
 
 n1 = float(input("Enter 1st number in calculation:"))
 n2 = float(input("Enter 2st number in calculation:"))
 
-if TypeOfArthematic == "multiplacation":
+if TypeOfArthematic == "m":
     print(f"The answer to your math problem is {n1*n2}.")
-elif TypeOfArthematic == "addition":
+elif TypeOfArthematic == "a":
     print(f"The answer to your math problem is {n1+n2}.")
-elif TypeOfArthematic == "subtraction":
+elif TypeOfArthematic == "s":
         print(f"The answer to your math problem is {n1-n2}.")
-elif TypeOfArthematic == "division":
+elif TypeOfArthematic == "d":
         if n2 == 0:
             print("You cannot divide a number by 0.")
         else:
             print(f"The answer to your math problem is {n1//n2}.")
-            print (f"The complete division is {n1/n2}.")
+            print (f"The complete d is {n1/n2}.")
             if n1 % n2 > 0:
                 print(f"The remainder is {n1%n2}.")
-elif TypeOfArthematic == "power":
-    print(f"The answer to {n1} to the power of {n2} is {n1**n2}.")
+elif TypeOfArthematic == "p":
+    print(f"The answer to {n1} to the p of {n2} is {n1**n2}.")
