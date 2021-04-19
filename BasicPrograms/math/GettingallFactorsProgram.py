a8cdb609bd068375d08c010607a5df8fd847a112
Smira C <76918800+smirachowdhary@users.noreply.github.com>
2021-04-19 08:04:12 -0700

    making it work

--- BasicPrograms/math/GettingallFactorsProgram.py
+++ BasicPrograms/math/GettingallFactorsProgram.py
@@ -1,13 +1,10 @@
 n = int(input("Enter number here:"))
+factors = ""
 
-def get_all_factors(number):
-    factors = ""
+for i in range(1,n+1):
+    if n % i == 0:
+        factor_input = f"{i} "
+        factors += factor_input
 
-    for i in range(1,number+1):
-        if number % i == 0:
-            factor_input = f"{i} "
-            factors += factor_input
 
-    return factors
-
-get_all_factors(n)
+print(factors)
