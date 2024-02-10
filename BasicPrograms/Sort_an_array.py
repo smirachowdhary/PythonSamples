7da7275a747435006bb14a23480b085cb0deb302
smira chowdhary <smirachowdhary@gmail.com>
2024-02-10 14:38:43 -0800

    editing

--- BasicPrograms/Sort_an_array.py
+++ BasicPrograms/Sort_an_array.py
@@ -5,10 +5,7 @@ def SortArray(array):
         j=0
         while j < len(array):
             if array[i]<array[j] and i<j:
-                x=array[i]
-                array[i]=array[j]
-                array[j]=x
-            if array[i]>array[j] and i>j:
+                print(f"A--{i}--{j}--{array[i]}--{array[j]}")
                 x=array[i]
                 array[i]=array[j]
                 array[j]=x
@@ -29,7 +26,8 @@ def FindAllIndexes(array,value):
     i=0
     while i<len(array):
         if array[i]==value:
-            index.append[i]
+            index.append(i)
+        i+=1
     return index
 
 def SearchInArray(array,value):
@@ -42,12 +40,33 @@ def SearchInArray(array,value):
         present = 'This element is not present in this array.'
     
     return present
+def FindANum(upper_limit, lower_limit, value):
+    
 
-testcase = [4,5,88,77,3,6,9,10,77]
-print(SortArray(testcase))
+def BinarySearch(array,value):
+    x=(len(array)-1)//2
+    index=0
+    i=0
+    while i<len(array):
+        if array[x]==value:
+            index=x
+            break
+        else:
+            if x > array.index(value):
+                x=(len(array)-x)/2
+            if x < array.index(value):
+                x=x/2
+        i+=1
+    return index
 
-testcase = [4,5,88,77,3,6,9,10,77]
-print(FindAllIndexes(testcase, 77))
+#testcase = [4,5,88,77,3,6,9,10,77]
+#print(SortArray(testcase))
 
-testcase = [4,5,88,77,3,6,9,10,77]
-print(SearchInArray(testcase, 3))
+#testcase = [4,5,88,77,3,6,9,10,77]
+#print(FindAllIndexes(testcase, 77))
+
+#testcase = [4,5,88,77,3,6,9,10,77]
+#print(SearchInArray(testcase, 3))
+
+#testcase = [4,5,88,77,3,6,9,10,77]
+#print(BinarySearch(testcase, 6))
